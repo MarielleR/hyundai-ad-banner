47d6e00aa55f250924de516fe54353838a0639e5
Marielle <marielle@theredmoon.ca>
2021-02-21 11:38:57 -0500

    frame 3 and GSAP animations added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // 1B Chevrons Appearing
-gsap.fromTo("#chevrons", { opacity: 0 }, { duration: 10, opacity: 1});
+gsap.fromTo("#chevrons", { opacity: 0 }, { duration: 5, opacity: 1});
+
+
+
+gsap.fromTo("#msg-2", {opacity: 0}, {duration: 7, opacity: 1});
+
+gsap.fromTo("#msg-1", {opacity: 1}, { duration: 7, opacity: 0 });
 
