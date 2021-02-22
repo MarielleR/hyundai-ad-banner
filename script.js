a9e4f7d3f2dba61f70b0dd8b5a221111bc23f949
Marielle <marielle@theredmoon.ca>
2021-02-22 15:40:03 -0500

    code commented

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
+// Timeline:
+// Please note it's my first time using the GSAP library, I loved it! So cool! 
 
-// 1B Chevrons Appearing
+// Chevrons Appearing
 gsap.fromTo("#chevrons", { opacity: 0 }, { duration: 5, opacity: 1 });
 
+// Main Message 1 being replaced by Main Message 2
+gsap.fromTo("#msg-2", { opacity: 0, duration: 7 }, { opacity: 1, ease: "sine.in", delay: 5 });
 
-gsap.set("#msg-3", { opacity: 0, duration: 35 });
-
-
-
-let messageChangeOne = gsap.fromTo("#msg-2", { opacity: 0, duration: 7 }, { opacity: 1, ease: "sine.in", delay: 5 });
-
-let messageChangeTwo = gsap.fromTo("#msg-1", { opacity: 1, duration: 7 }, { opacity: 0, ease: "sine.out", delay: 5 });
+gsap.fromTo("#msg-1", { opacity: 1, duration: 7 }, { opacity: 0, ease: "sine.out", delay: 5 });
 
 
-
+// Preferred FWD features running through their sequence
 gsap.fromTo("#frame-4-a", { opacity: 1, duration: 8 }, { opacity: 0, ease: "sine.out", delay: 10 });
 
 gsap.fromTo("#frame-4-c", { opacity: 0, duration: 8 }, { opacity: 1, ease: "sine.out", delay: 10, y: -18, onComplete: hideAnimationOne });
@@ -31,13 +29,6 @@ function hideAnimationTwo() {
 }
 
 
-
-// function displayLastFrame() {
-
-// gsap.fromTo("#msg-3", { opacity: 0, ease: "sine.in", delay: 5 }, { opacity: 1, ease: "sine.in", delay: 5 });
-//     console.log("last frame");
-// }
-
 function hideAnimationThree() {
     gsap.to("#msg-2", { opacity: 0, delay: 5 })
     console.log("fireing");
@@ -45,12 +36,9 @@ function hideAnimationThree() {
 
 gsap.fromTo("#frame-5", { opacity: 0, duration: 10 }, { opacity: 1, ease: "sine.in", delay: 22, y: -30, onComplete: hideAnimationThree });
 
-// function displayLastFrame() {
-//     gsap.fromTo("#msg-3",{opacity:0, delay: 15}, { opacity: 1, ease: "sine.in", delay: 5});
-//     console.log("last frame");
-// }
 
-// gsap.set("#msg-3", {opacity: 0, duration: 10 }, {opacity: 1, ease: "sine-in", delay:2});
+// Main Message #3 set to appear quickly, ending the series of frames
+gsap.set("#msg-3", { opacity: 0, duration: 35 });
 
 gsap.set("#msg-3",  { opacity: 1,  delay: 29 });
 
