058ee1620ea9d1a9dc5595728af3ac9c96e303a9
Marielle <marielle@theredmoon.ca>
2021-02-22 15:16:16 -0500

    completed code

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ let messageChangeTwo = gsap.fromTo("#msg-1", { opacity: 1, duration: 7 }, { opac
 
 gsap.fromTo("#frame-4-a", { opacity: 1, duration: 8 }, { opacity: 0, ease: "sine.out", delay: 10 });
 
-gsap.fromTo("#frame-4-c", { opacity: 0, duration: 8 }, { opacity: 1, ease: "sine.out", delay: 10, y: -23, onComplete: hideAnimationOne });
+gsap.fromTo("#frame-4-c", { opacity: 0, duration: 8 }, { opacity: 1, ease: "sine.out", delay: 10, y: -18, onComplete: hideAnimationOne });
 
 gsap.fromTo("#frame-4-b", { opacity: 1, duration: 8 }, { opacity: 0, ease: "sine.out", delay: 10 });
 
-gsap.fromTo("#frame-4-d", { opacity: 0, duration: 8 }, { opacity: 1, ease: "sine.out", delay: 10, y: -23, onComplete: hideAnimationTwo });
+gsap.fromTo("#frame-4-d", { opacity: 0, duration: 8 }, { opacity: 1, ease: "sine.out", delay: 10, y: -18, onComplete: hideAnimationTwo });
 
 function hideAnimationOne() {
     gsap.to("#frame-4-c", { opacity: 0, delay: 10 })
@@ -43,7 +43,7 @@ function hideAnimationThree() {
     console.log("fireing");
 }
 
-gsap.fromTo("#frame-5", { opacity: 0, duration: 10 }, { opacity: 1, ease: "sine.in", delay: 22, y: -42, onComplete: hideAnimationThree });
+gsap.fromTo("#frame-5", { opacity: 0, duration: 10 }, { opacity: 1, ease: "sine.in", delay: 22, y: -30, onComplete: hideAnimationThree });
 
 // function displayLastFrame() {
 //     gsap.fromTo("#msg-3",{opacity:0, delay: 15}, { opacity: 1, ease: "sine.in", delay: 5});
@@ -52,7 +52,7 @@ gsap.fromTo("#frame-5", { opacity: 0, duration: 10 }, { opacity: 1, ease: "sine.
 
 // gsap.set("#msg-3", {opacity: 0, duration: 10 }, {opacity: 1, ease: "sine-in", delay:2});
 
-gsap.set("#msg-3",  { opacity: 1,  delay: 27 });
+gsap.set("#msg-3",  { opacity: 1,  delay: 29 });
 
 
 
